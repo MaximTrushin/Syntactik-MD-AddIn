a8a4df05c6332fe0c51092fca1e6d2b606fb9542
Maxim Trushin <trushin@gmail.com>
2017-05-12 20:25:09 -0400

    Added prototype logic for multiline string highlighting.

--- src/SyntactikMDAddin/Highlighting/SyntactikSpanParser.cs
+++ src/SyntactikMDAddin/Highlighting/SyntactikSpanParser.cs
@@ -15,7 +15,45 @@ namespace Syntactik.MonoDevelop.Highlighting
 
         protected override bool ScanSpan(ref int i)
         {
+            if (CurRule != null && CurRule.Name == "open_string" && !(CurSpan is IndentSpan))
+            {
+                var indent = CurText.TakeWhile(c => c == ' ' || c == '\t').Count();
+                FoundSpanBegin(new IndentSpan() {Indent = indent, FirstLine = true, Rule = "open_string", Color = "String"}, i, 0);
+                return true;
+
+            }
+
             return base.ScanSpan(ref i);
         }
+
+        protected override bool ScanSpanEnd(Span cur, ref int i)
+        {
+            if (CurRule != null && CurRule.Name == "open_string" && CurSpan is IndentSpan)
+            {
+                var indentSpan = (IndentSpan) CurSpan;
+                if (indentSpan.FirstLine && i == StartOffset)
+                {
+                    indentSpan.FirstLine = false;
+                }
+
+                if (!indentSpan.FirstLine)
+                {
+                    var indent = CurText.TakeWhile(c => c == ' ' || c == '\t').Count();
+                    if (indent <= indentSpan.Indent)
+                    {
+                        FoundSpanEnd(CurSpan, i, 0);
+                        return true;
+                    }
+                }
+            }
+            return base.ScanSpanEnd(cur, ref i);
+        }
     }
+
+    public class IndentSpan : Span
+    {
+        public int Indent;
+        public bool FirstLine;
+    }
+
 }
--- src/SyntactikMDAddin/Highlighting/SyntactikSyntaxMode.cs
+++ src/SyntactikMDAddin/Highlighting/SyntactikSyntaxMode.cs
@@ -1,4 +1,6 @@
-﻿using Mono.TextEditor;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mono.TextEditor;
 using Mono.TextEditor.Highlighting;
 
 namespace Syntactik.MonoDevelop.Highlighting
@@ -7,6 +9,20 @@ namespace Syntactik.MonoDevelop.Highlighting
     {
         public SyntactikSyntaxMode(TextDocument doc) : base(doc)
         {
+            ResourceStreamProvider provider = new ResourceStreamProvider(typeof(SyntactikSyntaxMode).Assembly, typeof(SyntactikSyntaxMode).Assembly.GetManifestResourceNames().First(s => s.Contains("SyntactikSyntaxMode")));
+            using (var stream = provider.Open())
+            {
+                SyntaxMode baseMode = SyntaxMode.Read(stream);
+                this.rules = new List<Rule>(baseMode.Rules);
+                this.keywords = new List<Keywords>(baseMode.Keywords);
+                this.spans = new List<Span>(baseMode.Spans).ToArray();
+                this.matches = baseMode.Matches;
+                this.prevMarker = baseMode.PrevMarker;
+                this.SemanticRules = new List<SemanticRule>(baseMode.SemanticRules);
+                this.keywordTable = baseMode.keywordTable;
+                this.keywordTableIgnoreCase = baseMode.keywordTableIgnoreCase;
+                this.properties = baseMode.Properties;
+            }
         }
 
         public override SpanParser CreateSpanParser(DocumentLine line, CloneableStack<Span> spanStack)
