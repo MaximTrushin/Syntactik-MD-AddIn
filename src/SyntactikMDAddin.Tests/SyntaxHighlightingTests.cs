cc2310f485db44be3ca1c30ff11fe2aa8087225f
Maxim Trushin <trushin@gmail.com>
2017-05-17 13:53:16 -0400

    Added test project.

---
+++ src/SyntactikMDAddin.Tests/SyntaxHighlightingTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.TextEditor;
+using Mono.TextEditor.Highlighting;
+using NUnit.Framework;
+using Syntactik.MonoDevelop.Highlighting;
+
+namespace SyntactikMDAddin.Tests
+{
+    [TestFixture]
+    public class SyntaxHighlightingTests : TextEditorTestBase
+    {
+        [Test]
+        public void ValidateSyntaxModes()
+        {
+            Assert.IsTrue(SyntaxModeService.ValidateAllSyntaxModes());
+        }
+
+        [Test]
+        public void TestSpans()
+        {
+            TestOutput("/* TestMe */",
+                        "<span foreground=\"#888A85\">/* TestMe */</span>");
+        }
+
+
+        static void TestOutput(string input, string expectedMarkup)
+        {
+            string markup = GetMarkup(input);
+            SaveMarkup(markup);
+
+            if (markup != expectedMarkup)
+            {
+                Console.WriteLine("----Expected:");
+                Console.WriteLine(expectedMarkup);
+                Console.WriteLine("----Got:");
+                Console.WriteLine(markup);
+            }
+            Assert.AreEqual(expectedMarkup, markup, "expected:" + expectedMarkup + Environment.NewLine + "But got:" + markup);
+        }
+
+        private static void SaveMarkup(string markup)
+        {
+            Directory.CreateDirectory(Path.Combine(AssemblyDirectory, "markup"));
+            File.WriteAllText(Path.Combine(AssemblyDirectory,"markup", GetTestCaseName() + ".html"), markup);
+        }
+
+        public static string GetMarkup(string input)
+        {
+            var data = new TextEditorData(new TextDocument(input));
+            var syntaxMode = new SyntactikSyntaxMode(data.Document);
+            data.Document.MimeType = "text/x-syntactik4xml";
+            data.Document.SyntaxMode = syntaxMode;
+            var schema = ColorScheme.LoadFrom(File.OpenRead(Path.Combine(AssemblyDirectory, "SyntactikColorSchema.json")));
+            SyntaxModeService.AddStyle(schema);
+            data.ColorStyle = SyntaxModeService.GetColorStyle("Syntactik");
+            return data.GetMarkup(0, data.Length, false).Replace("foreground=\"", "style=\"color:");
+        }
+    }
+}
---
+++ src/SyntactikMDAddin.Tests/TextEditorTestBase.cs
@@ -0,0 +1,197 @@
+﻿// 
+// TextEditorTestBase.cs
+//  
+// Author:
+//       Mike Krüger <mkrueger@xamarin.com>
+// 
+// Copyright (c) 2012 Xamarin Inc. (http://xamarin.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Mono.TextEditor;
+
+namespace SyntactikMDAddin.Tests
+{
+    public class TextEditorTestBase
+    {
+        static bool firstRun = true;
+
+        [OneTimeSetUp]
+        public virtual void Setup()
+        {
+            if (firstRun)
+            {
+                Gtk.Application.Init();
+                firstRun = false;
+            }
+        }
+
+        [OneTimeTearDown]
+        public virtual void TearDown()
+        {
+        }
+        protected static string AssemblyDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static TextEditorData Create(string content, ITextEditorOptions options = null)
+        {
+            var data = new TextEditorData();
+            if (options != null)
+                data.Options = options;
+            var sb = new StringBuilder();
+            int caretIndex = -1, selectionStart = -1, selectionEnd = -1;
+            var foldSegments = new List<FoldSegment>();
+            var foldStack = new Stack<FoldSegment>();
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                var ch = content[i];
+                switch (ch)
+                {
+                    case '$':
+                        caretIndex = sb.Length;
+                        break;
+                    case '<':
+                        if (i + 1 < content.Length)
+                        {
+                            if (content[i + 1] == '-')
+                            {
+                                selectionStart = sb.Length;
+                                i++;
+                                break;
+                            }
+                        }
+                        goto default;
+                    case '-':
+                        if (i + 1 < content.Length)
+                        {
+                            var next = content[i + 1];
+                            if (next == '>')
+                            {
+                                selectionEnd = sb.Length;
+                                i++;
+                                break;
+                            }
+                            if (next == '[')
+                            {
+                                var segment = new FoldSegment(data.Document, "...", sb.Length, 0, FoldingType.None)
+                                {
+                                    IsFolded = false
+                                };
+                                foldStack.Push(segment);
+                                i++;
+                                break;
+                            }
+                        }
+                        goto default;
+                    case '+':
+                        if (i + 1 < content.Length)
+                        {
+                            var next = content[i + 1];
+                            if (next == '[')
+                            {
+                                var segment = new FoldSegment(data.Document, "...", sb.Length, 0, FoldingType.None)
+                                {
+                                    IsFolded = true
+                                };
+                                foldStack.Push(segment);
+                                i++;
+                                break;
+                            }
+                        }
+                        goto default;
+                    case ']':
+                        if (foldStack.Count > 0)
+                        {
+                            FoldSegment segment = foldStack.Pop();
+                            segment.Length = sb.Length - segment.Offset;
+                            foldSegments.Add(segment);
+                            break;
+                        }
+                        goto default;
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+
+            data.Text = sb.ToString();
+
+            if (caretIndex >= 0)
+                data.Caret.Offset = caretIndex;
+            if (selectionStart >= 0)
+            {
+                if (caretIndex == selectionStart)
+                {
+                    data.SetSelection(selectionEnd, selectionStart);
+                }
+                else
+                {
+                    data.SetSelection(selectionStart, selectionEnd);
+                    if (caretIndex < 0)
+                        data.Caret.Offset = selectionEnd;
+                }
+            }
+            if (foldSegments.Count > 0)
+                data.Document.UpdateFoldSegments(foldSegments);
+            return data;
+        }
+
+        public static void Check(TextEditorData data, string content)
+        {
+            var checkDocument = Create(content);
+            if (checkDocument.Text != data.Text)
+            {
+                Console.WriteLine("was:");
+                Console.WriteLine(data.Text);
+                Console.WriteLine("expected:");
+                Console.WriteLine(checkDocument.Text);
+            }
+            Assert.AreEqual(checkDocument.Text, data.Text);
+            Assert.AreEqual(checkDocument.Caret.Offset, data.Caret.Offset, "Caret offset mismatch.");
+            if (data.IsSomethingSelected || checkDocument.IsSomethingSelected)
+                Assert.AreEqual(checkDocument.SelectionRange, data.SelectionRange, "Selection mismatch.");
+            if (checkDocument.Document.HasFoldSegments || data.Document.HasFoldSegments)
+            {
+                var list1 = new List<FoldSegment>(checkDocument.Document.FoldSegments);
+                var list2 = new List<FoldSegment>(data.Document.FoldSegments);
+                Assert.AreEqual(list1.Count, list2.Count, "Fold segment count mismatch.");
+                for (int i = 0; i < list1.Count; i++)
+                {
+                    Assert.AreEqual(list1[i].Segment, list2[i].Segment, "Fold " + i + " segment mismatch.");
+                    Assert.AreEqual(list1[i].IsFolded, list2[i].IsFolded, "Fold " + i + " isFolded mismatch.");
+                }
+            }
+        }
+
+        protected static string GetTestCaseName()
+        {
+            var trace = new StackTrace();
+            return trace.GetFrames().Select(f => f.GetMethod()).First(m => m.CustomAttributes.Any(a => a.AttributeType.FullName == "NUnit.Framework.TestAttribute")).Name;
+        }
+    }
+}
+
