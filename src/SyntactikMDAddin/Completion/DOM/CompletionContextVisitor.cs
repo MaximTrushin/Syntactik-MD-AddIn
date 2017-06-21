7728816fef6c9a3d717bc212df69872310bec297
Maxim Trushin <trushin@gmail.com>
2017-06-21 12:37:59 -0400

    Added CompletionContextVisitor class.

---
+++ src/SyntactikMDAddin/Completion/DOM/CompletionContextVisitor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syntactik.DOM;
+
+namespace Syntactik.MonoDevelop.Completion.DOM
+{
+    public class CompletionContextVisitor: SyntactikDepthFirstVisitor
+    {
+        private Pair _lastPair;
+
+        public Pair LastPair
+        {
+            get { return _lastPair; }
+        }
+
+        public override void OnDocument(Syntactik.DOM.Document pair)
+        {
+            _lastPair = pair;
+            base.OnDocument(pair);
+        }
+
+        public override void OnElement(Syntactik.DOM.Element pair)
+        {
+            _lastPair = pair;
+            base.OnElement(pair);
+        }
+
+
+
+        public override void OnAttribute(Syntactik.DOM.Attribute pair)
+        {
+            _lastPair = pair;
+            base.OnAttribute(pair);
+        }
+
+        public override void OnAlias(Syntactik.DOM.Alias pair)
+        {
+            _lastPair = pair;
+            base.OnAlias(pair);
+        }
+        public override void OnArgument(Syntactik.DOM.Argument pair)
+        {
+            _lastPair = pair;
+            base.OnArgument(pair);
+        }
+        public override void OnParameter(Syntactik.DOM.Parameter pair)
+        {
+            _lastPair = pair;
+            base.OnParameter(pair);
+        }
+
+        public override void OnAliasDefinition(Syntactik.DOM.AliasDefinition pair)
+        {
+            _lastPair = pair;
+            base.OnAliasDefinition(pair);
+        }
+
+        public override void OnNamespaceDefinition(Syntactik.DOM.NamespaceDefinition pair)
+        {
+            _lastPair = pair;
+            base.OnNamespaceDefinition(pair);
+        }
+
+        public override void OnScope(Syntactik.DOM.Scope pair)
+        {
+            _lastPair = pair;
+            base.OnScope(pair);
+        }
+    }
+}
--- src/SyntactikMDAddin/Completion/SyntactikCompletionTextEditorExtension.cs
+++ src/SyntactikMDAddin/Completion/SyntactikCompletionTextEditorExtension.cs
@@ -3,6 +3,10 @@ using System.Threading.Tasks;
 using MonoDevelop.Ide.CodeCompletion;
 using MonoDevelop.Ide.Editor;
 using MonoDevelop.Ide.Editor.Extension;
+using Syntactik.Compiler;
+using Syntactik.Compiler.IO;
+using Syntactik.MonoDevelop.Completion.DOM;
+using Syntactik.MonoDevelop.Parser;
 
 namespace Syntactik.MonoDevelop.Completion
 {
@@ -14,12 +18,22 @@ namespace Syntactik.MonoDevelop.Completion
         {
             base.Initialize();
         }
+
         public override void Dispose()
         {
             base.Dispose();
         }
 
-        public override Task<ICompletionDataList> HandleCodeCompletionAsync(CodeCompletionContext completionContext, char completionChar, CancellationToken token = default(CancellationToken))
+        public override Task<ICompletionDataList> CodeCompletionCommand(CodeCompletionContext completionContext)
+        {
+            int pos = completionContext.TriggerOffset;
+            if (pos <= 0)
+                return null;
+            return HandleCodeCompletion(completionContext, true, default(CancellationToken));
+        }
+
+        public override Task<ICompletionDataList> HandleCodeCompletionAsync(CodeCompletionContext completionContext,
+            char completionChar, CancellationToken token = default(CancellationToken))
         {
             int pos = completionContext.TriggerOffset;
             char ch = Editor.GetCharAt(pos - 1);
@@ -35,29 +49,48 @@ namespace Syntactik.MonoDevelop.Completion
             CodeCompletionContext completionContext, bool forced, CancellationToken token)
         {
             var buf = this.Editor;
-            var currentLocation = new DocumentLocation(completionContext.TriggerLine, completionContext.TriggerLineOffset);
-            char currentChar = completionContext.TriggerOffset < 1 ? ' ' : buf.GetCharAt(completionContext.TriggerOffset - 1);
-            char previousChar = completionContext.TriggerOffset < 2 ? ' ' : buf.GetCharAt(completionContext.TriggerOffset - 2);
+            var compilerParameters = CreateCompilerParameters(Editor.FileName, Editor.Text, Editor.CaretOffset);
+            var compiler = new SyntactikCompiler(compilerParameters);
+            var context = compiler.Run();
 
-            if (currentChar == '$')
-            {
-                var list = new CompletionDataList();
+            var currentLocation = new DocumentLocation(completionContext.TriggerLine,
+                completionContext.TriggerLineOffset);
+            char currentChar = completionContext.TriggerOffset < 1
+                ? ' '
+                : buf.GetCharAt(completionContext.TriggerOffset - 1);
+            char previousChar = completionContext.TriggerOffset < 2
+                ? ' '
+                : buf.GetCharAt(completionContext.TriggerOffset - 2);
 
-                list.Add("Parsing file").Description = "parsing ...";
-                return list;
-            }
-            else
-            {
-                
-            }
+            var visitor = new CompletionContextVisitor();
+            visitor.Visit(context.CompileUnit);
+            var p = visitor.LastPair;
+
+            //if (currentChar == '$')
+            //{
+            //    var list = new CompletionDataList();
+
+            //    list.Add("Parsing file").Description = "parsing ...";
+            //    return list;
+            //}
+
 
             return null;
         }
 
-        //public override Task<ICompletionDataList> CodeCompletionCommand(CodeCompletionContext completionContext)
+        private CompilerParameters CreateCompilerParameters(string fileName, string content, int offset,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            var compilerParameters = new CompilerParameters {Pipeline = new CompilerPipeline()};
+            compilerParameters.Pipeline.Steps.Add(new ParseForCompletionStep(cancellationToken));
+            compilerParameters.Input.Add(new StringInput(fileName, content.Substring(0, offset + 1)));
+            return compilerParameters;
+
+            //public override Task<ICompletionDataList> CodeCompletionCommand(CodeCompletionContext completionContext)
             //{
             //    //return InternalHandleCodeCompletion(completionContext, ch, true, triggerWordLength, default(CancellationToken));
             //    return Task.FromResult(GetCompletionList(completionContext));
             //}
         }
+    }
 }
