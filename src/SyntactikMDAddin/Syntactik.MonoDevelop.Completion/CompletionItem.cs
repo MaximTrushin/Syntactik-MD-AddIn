ceaac5bc5f59d04caeed5f9132a5e4290db9e0e8
Maxim Trushin <trushin@gmail.com>
2017-08-02 19:28:31 -0400

    Added ability to assign PriorityGroup to CompletionItem.
    It allows sorting in completion window.

--- src/SyntactikMDAddin/Syntactik.MonoDevelop.Completion/CompletionItem.cs
+++ src/SyntactikMDAddin/Syntactik.MonoDevelop.Completion/CompletionItem.cs
@@ -1,4 +1,5 @@
-﻿using MonoDevelop.Ide.CodeCompletion;
+﻿using MonoDevelop.Components.Chart;
+using MonoDevelop.Ide.CodeCompletion;
 using MonoDevelop.Ide.Editor.Extension;
 
 namespace Syntactik.MonoDevelop.Completion
@@ -15,15 +16,31 @@ namespace Syntactik.MonoDevelop.Completion
     }
     public class CompletionItem : CompletionData
     {
+        private int _priorityGroup;
         public ItemType ItemType { get; set; }
         public bool UndeclaredNamespaceUsed { get; set; }
         public string Namespace { get; set; }
         public string NsPrefix { get; set; }
+        public override int PriorityGroup => _priorityGroup;
+
+        public int Priority
+        {
+            set { _priorityGroup = value; }
+        }
 
         public override void InsertCompletionText(CompletionListWindow window, ref KeyActions ka, KeyDescriptor descriptor)
         {
             ((SyntactikCompletionTextEditorExtension)window.Extension).SelectedCompletionItem = this;
             base.InsertCompletionText(window, ref ka, descriptor);
+        }
+
+        public override int CompareTo(object obj)
+        {
+            var item = obj as CompletionData;
+            if (item == null)
+                return 0;
+            var result = item.PriorityGroup.CompareTo(PriorityGroup);
+            return result != 0 ? result : Compare(this, (CompletionData) obj);
         }
     }
 }
