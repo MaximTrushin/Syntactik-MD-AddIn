89b2e04bbef6c45bfea7f08377606c57c10ba535
Maxim Trushin <trushin@gmail.com>
2017-10-02 10:26:35 -0400

    Added projects SyntactikGtk and Syntactik.Swagger.

---
+++ src/SyntactikGtk/Syntactik.Swagger/Client/ApiClientConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO.Swagger.Client
+{
+    public partial class ApiClient
+    {
+        public static string Address = "https://malinalicensing.azurewebsites.net/api/subscription";
+    }
+}
--- src/SyntactikMDAddin/Syntactik.MonoDevelop.Commands/LicenseInfoHandler.cs
+++ src/SyntactikMDAddin/Syntactik.MonoDevelop.Commands/LicenseInfoHandler.cs
@@ -1,8 +1,19 @@
-﻿using MonoDevelop.Components.Commands;
+﻿using Gtk;
+using MonoDevelop.Components.Commands;
+using Syntactik.MonoDevelop.License;
 
 namespace Syntactik.MonoDevelop.Commands
 {
     public class LicenseInfoHandler : CommandHandler
     {
+        protected override void Run()
+        {
+            base.Run();
+            var dlg = new LicenseInfo();
+            dlg.SetPosition(WindowPosition.CenterAlways);
+            var res = (ResponseType)dlg.Run();
+            dlg.Hide();
+        }
+
     }
 }
