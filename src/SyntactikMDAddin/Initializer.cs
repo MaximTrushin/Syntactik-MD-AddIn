a299facfb11a884031bcdd0d5f12e70a5122a8c8
Maxim Trushin <trushin@gmail.com>
2017-11-01 18:42:23 -0400

    Added menu ReportSyntactikBug.
    Add addin Initializer class.

---
+++ src/SyntactikMDAddin/Initializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+
+namespace Syntactik.MonoDevelop
+{
+    internal class Initializer : CommandHandler
+    {
+
+        protected override void Run()
+        {
+            IdeApp.CommandService.RegisterGlobalHandler(new GlobalCommandHandler());
+        }
+    }
+
+    class GlobalCommandHandler
+    {
+        // This handler will hide the Run menu if there are no debuggers installed.
+
+        [CommandHandler("ReportBug")]
+        public void OnRun()
+        {
+        }
+
+        [CommandUpdateHandler("ReportBug")]
+        public void OnRunUpdate(CommandInfo cinfo)
+        {
+            cinfo.Visible = true;
+          
+        }
+    }
+}
