f851e03904d601ea6c42b159f43fa5d8ea78523e
Maxim Trushin <trushin@gmail.com>
2017-11-02 18:08:48 -0400

    Initial implementation for Syntactik branded welcome page.

---
+++ src/SyntactikMDAddin/WelcomePage/SyntactikWelcomePage.cs
@@ -0,0 +1,44 @@
+﻿using Gtk;
+using MonoDevelop.Core;
+using MonoDevelop.Ide.WelcomePage;
+
+namespace Syntactik.MonoDevelop.WelcomePage
+{
+    class SyntactikWelcomePage : WelcomePageWidget
+    {
+        protected override void BuildContent(Container parent)
+        {
+
+            LogoImage = Xwt.Drawing.Image.FromFile(BrandingService.GetFile("welcome-logo.png"));
+            TopBorderImage = Xwt.Drawing.Image.FromFile(BrandingService.GetFile("welcome-tile.png"));
+
+            var mainAlignment = new Gtk.Alignment(0.5f, 0.5f, 0f, 1f);
+            var mainCol = new WelcomePageColumn();
+            mainAlignment.Add(mainCol);
+
+            var row1 = new WelcomePageRow();
+            row1.PackStart(new WelcomePageButtonBar(
+                new WelcomePageBarButton("Syntactik.com", "http://www.syntactik.com", "welcome-link-md-16.png"),
+                new WelcomePageBarButton(GettextCatalog.GetString("Documentation"), "https://syntactik.github.io/Malina/", "welcome-link-info-16.png"),
+                new WelcomePageBarButton(GettextCatalog.GetString("Support"), "https://gitter.im/syntactik-Malina/Lobby", "welcome-link-support-16.png")
+                )
+            );
+            mainCol.PackStart(row1, false, false, 0);
+
+            var row2 = new WelcomePageRow(
+                new WelcomePageColumn(
+                new WelcomePageRecentProjectsList(GettextCatalog.GetString("Solutions"))
+                ),
+                new WelcomePageColumn(
+                    new WelcomePageNewsFeed(GettextCatalog.GetString("Syntactik News"), "http://software.xamarin.com/Service/News", "NewsLinks")
+                ),
+                new WelcomePageColumn(
+                    new WelcomePageTipOfTheDaySection()
+                )
+            );
+            mainCol.PackStart(row2, false, false, 0);
+
+            parent.Add(mainAlignment);
+        }
+    }
+}
---
+++ src/SyntactikMDAddin/WelcomePage/WelcomePageProvider.cs
@@ -0,0 +1,13 @@
+﻿using MonoDevelop.Components;
+using MonoDevelop.Ide.WelcomePage;
+
+namespace Syntactik.MonoDevelop.WelcomePage
+{
+    public class WelcomePageProvider: IWelcomePageProvider
+    {
+        public Control CreateWidget()
+        {
+            return new SyntactikWelcomePage();
+        }
+    }
+}
