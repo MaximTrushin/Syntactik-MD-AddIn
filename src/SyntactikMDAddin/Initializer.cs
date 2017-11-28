590c6ea7860f38db782afc37456f325634104154
Maxim Trushin <trushin@gmail.com>
2017-11-28 18:38:54 -0500

    Integrated add-in with the readme on github.

--- src/SyntactikMDAddin/Initializer.cs
+++ src/SyntactikMDAddin/Initializer.cs
@@ -1,8 +1,13 @@
-﻿using MonoDevelop.Components.Commands;
+﻿using System.Threading.Tasks;
+using MonoDevelop.Components.Commands;
 using MonoDevelop.Core;
 using MonoDevelop.Ide;
 using MonoDevelop.Ide.Commands;
+using Syntactik.DOM.Mapped;
+using Syntactik.MonoDevelop.Completion;
 using Syntactik.MonoDevelop.Dialogs;
+using Syntactik.MonoDevelop.Licensing;
+using Syntactik.MonoDevelop.Projects;
 
 namespace Syntactik.MonoDevelop
 {
@@ -45,7 +50,64 @@ namespace Syntactik.MonoDevelop
         [CommandHandler(HelpCommands.Help)]
         public void HelpResolver()
         {
-            //widget.MonodocResolver();
+            var textEditor = IdeApp.Workbench.ActiveDocument?.Editor;
+            if (textEditor == null) return;
+
+            var ext = textEditor.GetContent<SyntactikCompletionTextEditorExtension>();
+            var task = ext.CompletionContextTask?.Task;
+            if (task != null)
+            {
+#if DEBUG
+                task.Wait(ext.CompletionContextTask.CancellationToken);
+#else
+                task.Wait(2000, ext.CompletionContextTask.CancellationToken);
+#endif
+                if (task.Status != TaskStatus.RanToCompletion) return;
+                CompletionContext context = task.Result;
+                
+                var lastPair = context.LastPair as IMappedPair;
+                if (lastPair == null) return;
+                if (lastPair is Argument)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#argument");
+                }
+                else if (lastPair is Alias)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#alias");
+                }
+                else if (lastPair is AliasDefinition)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#alias-definition");
+                }
+                else if (lastPair is Attribute)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#attribute");
+                }
+                else if (lastPair is Document)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#document");
+                }
+                else if (lastPair is Element)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#element");
+                }
+                else if (lastPair is Module)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#module");
+                }
+                else if (lastPair is NamespaceDefinition)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#namespace-definition");
+                }
+                else if (lastPair is Parameter)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#parameter");
+                }
+                else if (lastPair is Scope)
+                {
+                    DesktopService.ShowUrl(@"https://github.com/syntactik/Syntactik/blob/master/README.md#namespace-scope");
+                }
+            }
         }
 
         [CommandUpdateHandler(HelpCommands.Help)]
@@ -55,7 +117,6 @@ namespace Syntactik.MonoDevelop
             {
                 cinfo.Bypass = true;
             }
-            //widget.MonodocResolverUpdate(cinfo);
         }
     }
 }
